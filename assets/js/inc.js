0a99c8299157aac40e65c4930ac97d5936d8e4d8
Jayanta Bhunia <jayanta.bhunia@acefone.com>
2026-08-12 22:17:12 +0530

    changes

--- assets/js/inc.js
+++ assets/js/inc.js
@@ -1,6 +1,9 @@
 async function loadComponent(id, file) {
   const response = await fetch(file);
   document.getElementById(id).innerHTML = await response.text();
+  const currentUrl = window.location.pathname.split('/').pop();
+  const currentMenu = document.querySelector(`a[href="${currentUrl}"]`) ?? document.querySelector(`a[href="index.html"]`);
+  currentMenu.className = "active";
 }
 
 loadComponent("header", 'inc/header.html');

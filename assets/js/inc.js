60885e14bafeee40e15c73f43f24bc31211ee3c4
Jayanta Bhunia <jayanta.bhunia@acefone.com>
2026-08-12 21:26:55 +0530

    changes

---
+++ assets/js/inc.js
@@ -0,0 +1,7 @@
+async function loadComponent(id, file) {
+  const response = await fetch(file);
+  document.getElementById(id).innerHTML = await response.text();
+}
+
+loadComponent("header", '/inc/header.html');
+loadComponent("footer", "/inc/footer.html");

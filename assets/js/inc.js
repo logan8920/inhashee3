7be827f6d1f5434c260934faece39a48557397d9
Jayanta Bhunia <jayanta.bhunia@acefone.com>
2026-08-14 18:37:31 +0530

    changes

--- assets/js/inc.js
+++ assets/js/inc.js
@@ -27,32 +27,4 @@ document.addEventListener("DOMContentLoaded", async function (e) {
     });
   }
 
-  //
-
-  try {
-    const res = await fetch(
-      "https://morning-dream-6de5.bhuniajayant.workers.dev/api-get-products?limit=4",
-    );
-
-    if (!res.ok) {
-      console.log(res);
-      throw new Error(res.statusText);
-    }
-
-    const products = await res.json();
-
-    console.log(products);
-
-    let div = "";
-    if (products?.data?.length) {
-      products.data.splice(0, 4).forEach((product) => {
-        div += `<li><a href="product.html?id="${product?.id}" alt="${product?.productName}">${product?.productName}</a></li>`;
-      });
-    } else {
-      div = "<li>No Product Available</li>";
-    }
-    footerProduct.innerHTML = div;
-  } catch (error) {
-    alert(error.message);
-  }
 });
